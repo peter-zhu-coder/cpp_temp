4bc30f54bbcdc6d58bac734cd4fe393da3658b7d
peter-zhu-coder <15800959605@163.com>
2023-06-18 14:35:07 +0800

    init

---
+++ test02.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <string.h>
+#include <stdio.h>
+#include <math.h>
+#include <unordered_set>
+#include <vector>
+
+int main() {
+    int score;
+    std::vector<int> data;
+    while (std::cin >> score) {
+        try {
+            if (score < 0 || score > 100) {
+                throw(score);
+            }
+            data.push_back(score);
+        }
+        catch (int score) {
+            std::cerr << "score illegal "<< std::endl;
+            break;
+        }
+    }
+    std::cout << "size :" << data.size() << std::endl;
+    std::cout << "task completed " << std::endl;
+    return 0;
+}
---
+++ test03.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <string.h>
+#include <stdio.h>
+#include <math.h>
+#include <unordered_set>
+#include <vector>
+#include <assert.h>
+using namespace std;
+
+int main() {
+    string a = "anfnrelfrjlfv";
+    string b;
+    ostringstream zhu;
+    zhu << a;
+    b = zhu.str();
+    // assert(0);
+
+    return 0;
+}
