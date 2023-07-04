fe938e313a1c13331c93c9640d4eace1c8f6f082
peter-zhu-coder <15800959605@163.com>
2023-07-04 10:03:02 +0800

    reorganize the repository structure

---
+++ 0703/test03.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <string.h>
+#include <stdio.h>
+#include <math.h>
+#include <unordered_set>
+#include <vector>
+#include <assert.h>
+#include <float.h>
+// using namespace std;
+
+int main() {
+    // int a = 8;
+    // int tmp;
+    // for(int i = 0; i < 32; i++) {
+    //     tmp = (a >> i) & 0x1;
+    //     std::cout << tmp << " ";
+    // }
+    // char a = 'a';
+    // character
+    // int 
+    // integr 
+    // long int 
+    // short int 
+    // float
+    // double 
+    // std::string s = "hello world";
+    // std::cout << s << std::endl;
+
+    // compare the function of memcmp and mem move 
+    char* a = "abc";
+    char* b = "acc";
+    int i = memcmp(a, b, 4);
+
+    
+    // system("pause");
+    return 0;
+}
+
+
+
+
+
+
+void func() {
+
+
+    return;
+}
+
