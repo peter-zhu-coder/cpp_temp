616ab3116adfc6a2ccee448a4b98497fc7a1e857
peter-zhu-coder <15800959605@163.com>
2023-07-09 16:07:04 +0800

    reformat file name

---
+++ test/test.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string.h>
+#include <math.h>
+#include <vector>
+#include <memory.h>
+
+int main() {
+    int x = 1;
+    int y = -x;
+    std::cout << y << std::endl;
+
+    return 0;
+}
