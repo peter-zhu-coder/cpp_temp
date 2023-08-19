4acae61c8d04f88f65d1ab4b54229d680884416c
peter-zhu-coder <15800959605@163.com>
2023-08-19 14:43:47 +0800

    modify test

---
+++ 0000_test/test.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string.h>
+#include <vector>
+ 
+int my_sum(int x, int y) {
+    return x + y;
+ }
+
+int main() {
+    // int arr1[5];
+    // int *arr2 = new int[5];
+    // arr2[0];
+    // std::cout << arr2 << "-"<< &arr2[0] << std::endl;
+    // std::cout << arr1 << "-"<< &arr1[0] << std::endl;
+    printf("\"\n");
+    return 0;
+}
